469f33f9c261b01d384ff8bf275936a087458aaa
Firoz Shaikh <fshaikh@mobiquityinc.com>
2016-02-22 12:08:46 +0530

    test project

---
+++ src/main/java/com/mobiquity/test/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.mobiquity.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld 
+{
+	private String name;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void printHello() {
+		System.out.println("Hello ! " + name);
+	}
+}
